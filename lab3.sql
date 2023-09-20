bf734bf0a001f5c6adda02b6c9714d6188915369
mjfolsom <143033905+mjfolsom@users.noreply.github.com>
2023-09-20 15:58:33 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -38,7 +38,7 @@ values ('c4', 'facebook', 'r1', 'p3@jmu.edu')
 -- you need to update the course first because p1 is actually teaching so you need to change that so that p3 can be switched
 update course 
 set p_email ='p3@jmu.edu'
-where p_email = 'p1@jmu.edu;
+where p_email = 'p1@jmu.edu'
 
 -- Q2.7 
 select * from enroll
